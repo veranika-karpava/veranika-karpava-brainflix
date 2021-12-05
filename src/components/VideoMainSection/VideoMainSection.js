ef8dbc40f964dc8dcb77eadae9e7bb012f6c3a5e
veranika-karpava <veranika.karpava@gmail.com>
2021-12-04 16:23:22 -0800

    Complete VideoMainSection

--- src/components/VideoMainSection/VideoMainSection.js
+++ src/components/VideoMainSection/VideoMainSection.js
@@ -1,7 +1,4 @@
 import './VideoMainSection.scss';
-// import fullscreen from '../../assets/images/fullscreen.svg';
-// import play from '../../assets/images/play.svg';
-// import volume_up from '../../assets/images/volume_up.svg';
 
 const VideoMainSection = (props) => {
     const { image, video } = props.selectedVideo;
