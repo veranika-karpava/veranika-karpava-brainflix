0b60afc6814bba808c8b1f1d6d802be868ec549f
veranika-karpava <veranika.karpava@gmail.com>
2021-12-05 16:23:41 -0800

    Complited existCommentBox

--- src/components/ExistCommentBox/ExistCommentBox.js
+++ src/components/ExistCommentBox/ExistCommentBox.js
@@ -4,16 +4,18 @@ const ExistCommentBox = (props) => {
 
     return (
         <div className='exist-comment-box'>
-            <div className='exist-comment-box__avatar-grey'></div>
             <ul className='exist-comment-box__list-comment'>
                 {props.comments.map((comment, i) => {
                     return (
                         <li className='exist-comment-box__item-comment' key={i}>
-                            <p className='exist-comment-box__author'>{comment.name}</p>
-                            <p className='exist-comment-box__datestamp'>
-                                {new Date(comment.timestamp).toLocaleDateString("en-US", { timeZone: "UTC", year: "numeric", month: "2-digit", day: "2-digit" })}
-                            </p>
-                            <p className='exist-comment-box__content'>{comment.comment}</p>
+                            <div className='exist-comment-box__avatar-grey'></div>
+                            <div className="exist-comment-box__content">
+                                <p className='exist-comment-box__author'>{comment.name}</p>
+                                <p className='exist-comment-box__datestamp'>
+                                    {new Date(comment.timestamp).toLocaleDateString("en-US", { timeZone: "UTC", year: "numeric", month: "2-digit", day: "2-digit" })}
+                                </p>
+                                <p className='exist-comment-box__text'>{comment.comment}</p>
+                            </div>
                         </li>
                     )
                 })}
